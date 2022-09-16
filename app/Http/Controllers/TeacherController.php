8405a6b23ac978fae32d787247118b7f228208aa
Mithu Hasan <engineermithubd@gmail.com>
2022-09-17 03:41:50 +0600

    teacher info update

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teacher;
+use Illuminate\Http\Request;
+use DB;
+
+class TeacherController extends Controller
+{
+    public function index(){
+
+        return view('teacher.index');
+    }
+
+    public function allData(){
+
+       $data = Teacher::latest()->get();
+
+       return response()->json($data);
+    }
+
+    public function storeData(Request $request){
+
+       $data = Teacher::insert([
+             'name' => $request->name,
+           'title' => $request->title,
+           'institute' =>$request->institute,
+       ]);
+
+       return response()->json($data);
+    }
+
+    public function editData($id){
+
+        $data  = Teacher::findOrFail($id);
+       return response()->json($data);
+    }
+
+    public function updateData(Request $request,$id){
+
+        $data = Teacher::findOrFail($id)->update([
+            'name' => $request->name,
+            'title' => $request->title,
+            'institute' =>$request->institute,
+        ]);
+       return response()->json($data);
+    }
+
+
+}
---
+++ resources/views/teacher/index.blade.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Laravel Ajax</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" type="text/javascript"></script>
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <!--Button icon-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div style="padding: 30px;"></div>
+<div class="container">
+    <h2 style="color: green;">
+        <marquee behavior="#" direction="#"> Laravel Ajax</marquee>
+    </h2>
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-border">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Department</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <span id="addT">Add new Teacher</span>
+                    <span id="UpdateT">Update Teacher</span>
+                </div>
+                <div class="card-body">
+{{--                    <form>--}}
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Title</label>
+                            <input type="text" class="form-control" id="title">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Institute</label>
+                            <input type="text" class="form-control" id="institute">
+                        </div>
+
+                        <input type="hidden" class="form-control" id="id" />
+                        <button type="submit" id="addB"class="btn btn-primary" onclick="addData()">Add</button>
+                        <button type="submit" id="updateB"class="btn btn-primary" onclick="updateData()">Update</button>
+{{--                    </form>--}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--JS-->
+<script>
+    $('#addT').show();
+    $('#UpdateT').hide();
+    $('#addB').show();
+    $('#updateB').hide();
+
+    $.ajaxSetup({
+        headers:{
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    })
+
+    function allData(){
+        $.ajax({
+            type: "GET",
+            dataType: 'json',
+            url: "/teacher/all",
+            success:function (response){
+                var data ="";
+                $.each(response, function (key,value){
+                    data = data + "<tr>"
+                    data = data + "<td>"+value.id+"</td>"
+                    data = data + "<td>"+value.name+"</td>"
+                    data = data + "<td>"+value.title+"</td>"
+                    data = data + "<td>"+value.institute+"</td>"
+                    data = data + "<td>"
+                    data = data + "<button class='btn btn-info text-light' onclick='editData("+value.id+")'>Edit</button>"
+                    data = data + "<button class='btn btn-danger m-2'>  Delete</button>"
+                    data = data + "</td>"
+                    data = data + "</tr>"
+                })
+                $('tbody').html(data);
+            }
+        })
+    }
+    allData();
+
+    function clearData(){
+        $('#name').val('');
+        $('#title').val('');
+        $('#institute').val('');
+    }
+    function addData(){
+        var name    =   $('#name').val();
+        var title   =   $('#title').val();
+        var institute   =   $('#institute').val();
+
+        $.ajax({
+            type: "POST",
+            dataType: 'json',
+            data: {name:name, title:title, institute:institute},
+            url: "/teacher/store/",
+            success: function (data){
+                allData();
+                clearData();
+                console.log('Successfully Data Added');
+            }
+        })
+    }
+
+    function editData(id){
+        $.ajax({
+            type: "GET",
+            dataType: 'json',
+            url: "/teacher/edit/"+id,
+            success: function (data){
+                $('#addT').hide();
+                $('#UpdateT').show();
+                $('#addB').hide();
+                $('#updateB').show();
+                $('#id').val(data.id);
+                $('#name').val(data.name);
+                $('#title').val(data.title);
+                $('#institute').val(data.institute);
+            }
+        })
+    }
+
+    function updateData(){
+        var id          =   $('#id').val();
+        var name        =   $('#name').val();
+        var title       =   $('#title').val();
+        var institute   =   $('#institute').val();
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {name:name, title:title, institute:institute},
+            url: "/teacher/update/"+id,
+            success: function (data){
+                $('#addT').show();
+                $('#UpdateT').hide();
+                $('#addB').show();
+                $('#updateB').hide();
+                allData();
+                clearData();
+                console.log('Successfully Data Update');
+            }
+        })
+
+    }
+
+</script>
+
+</body>
+</html>
